c9d772e1bc38771f04012d37f98d483bec3dea06
rsartpl <rrsartpl@gmail.com>
2022-04-11 19:15:10 +0200

    test2

---
+++ src/main/java/pl/rafaelo/movielibrary/movielibrary/MovieController.java
@@ -0,0 +1,20 @@
+package pl.rafaelo.movielibrary.movielibrary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+    @RestController
+    public class MovieController {
+
+        @GetMapping("/test")
+        public int test() {
+
+            return 1;
+
+        }
+
+    }
+
+
